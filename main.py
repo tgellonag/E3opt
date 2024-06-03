bdc53e9319b7a2e00040e2598ea06635fe7a9322
Martin Muñoz Varas <127346279+mmunozv@users.noreply.github.com>
2024-06-03 22:50:50 +0000

    coment

--- main.py
+++ main.py
@@ -14,6 +14,18 @@ elif opcion == '3':
 elif opcion == '4':
     carpeta_parametros = carpetas_parametros[3]
 
+
+# Primero resolvemos el problema multiplicando las velocidades para trabajar con menos intervalos de tiempo.
+
+# Para resolver este problema se utiliza una cota superior para T equivalente a la cantidad de periodos de tiempo
+# que le tomaría a cada curso pasar por todos los pasillos uno a la vez, lo cual sirve como cota superior al problema
+# ya que se espera que un curso no tenga que pasar por todos los pasillos y que los cursos pueden estar recorriendo 
+# pasillos al mismo tiempo.
 tiempo_max = m.funcion( carpeta_parametros=carpeta_parametros, multiplicador= 5 )
 
+# Con este resultado, podemos definir una mejor cota superior para el tiempo máximo que los cursos se demorarían en evacuar
+# para así utilizar esta cota para el modelo que se busca optimizar (con periodos de 5s).
 m.funcion(carpeta_parametros=carpeta_parametros, multiplicador=1, tiempo_max=tiempo_max, final=True)
+
+# Todo este proceso se realiza con el fin de acortar el tiempo de ejecución del programa, para cumplir con el objetivo de
+# resolver el modelo en menos de 30 min desde que se ejecuta main.py y se selecciona la opcion 'parametros_dsla_parvulario'.

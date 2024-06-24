0d6139a546cd240f6cce2d736eb48e3bf2807b88
TOMAS GELLONA <tgellonag@uc.cl>
2024-06-23 20:07:28 -0400

    pdf

--- animacion2.py
+++ animacion2.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import json
 from copy import deepcopy
+from matplotlib.backends.backend_pdf import PdfPages
 
 def find_floor(lst, target):
     """Find the largest number in lst that is less than or equal to the target."""
@@ -37,35 +38,25 @@ def plot_static_elements(ax, pasillos, cursos):
     ax.clear()
 
     for i in pasillos:
-
         ax.plot([pasillos[i][0][0], pasillos[i][1][0]], [pasillos[i][0][1], pasillos[i][1][1]], 'k-')
 
-    ax.fill([0, 0, -5, -5], [0, 20, 20, 0], 'gray') # zs 3
-    ax.fill([4, 14, 14, 4], [0, 0, -5, -5], 'gray') # zs 2
-    ax.fill([34, 30, 30, 45, 45], [14, 19, 24, 24, 14], 'gray') #zs 1
+    ax.fill([0, 0, -6, -6], [0, 20, 20, 0], 'gray')  # zs 3
+    ax.fill([4, 14, 14, 4], [0, 0, -5, -5], 'gray')  # zs 2
+    ax.fill([34, 30, 30, 45, 45], [14, 19, 24, 24, 14], 'gray')  # zs 1
 
     # Escribir el origen de todos los cursos
-    
     for c in cursos:
-            
         ax.plot([pasillos[cursos[c][list(cursos[c].keys())[0]]][0][0]], [pasillos[cursos[c][list(cursos[c].keys())[0]]][0][1]], 'go')
 
-    
-
-
-def update_plot(t, ax, intervalos, cursos, pasillos, colors_markers, pos_zs = None):
-
-    pos_zs_copy = deepcopy(pos_zs)
+def update_plot(t, ax, intervalos, cursos, pasillos, colors_markers, pos_zs=None):
     """Update the plot for the given time step."""
+    pos_zs_copy = deepcopy(pos_zs)
 
     plot_static_elements(ax, pasillos, cursos)
 
     for c in cursos:
-
         periodo = t // 10
-
         if periodo >= intervalos[c][0]:
-
             periodo = find_floor(intervalos[c], periodo)
             next_time = intervalos[c][intervalos[c].index(periodo) + 1] if intervalos[c].index(periodo) + 1 < len(intervalos[c]) else 15
             dif_t = next_time - periodo
@@ -76,65 +67,51 @@ def update_plot(t, ax, intervalos, cursos, pasillos, colors_markers, pos_zs = No
             index_previo = cursos[c][prev_period] if prev_period is not None else None
 
             if index >= 0:
-
                 if index_previo is not None:
-
-                    if pasillos[index][0] == pasillos[index_previo][0]:                  
-
+                    if pasillos[index][0] == pasillos[index_previo][0]:
                         x = (1 - lan) * pasillos[index][0][0] + lan * pasillos[index][1][0]
                         y = (1 - lan) * pasillos[index][0][1] + lan * pasillos[index][1][1]
-                        
                     elif pasillos[index][0] == pasillos[index_previo][1]:
-
                         x = lan * pasillos[index][1][0] + (1 - lan) * pasillos[index][0][0]
                         y = lan * pasillos[index][1][1] + (1 - lan) * pasillos[index][0][1]
-
                     elif pasillos[index][1] == pasillos[index_previo][0]:
-
                         x = lan * pasillos[index][0][0] + (1 - lan) * pasillos[index][1][0]
                         y = lan * pasillos[index][0][1] + (1 - lan) * pasillos[index][1][1]
-                    
                     else:
-
                         x = (lan) * pasillos[index][0][0] + (1-lan) * pasillos[index][1][0]
                         y = (lan) * pasillos[index][0][1] + (1-lan) * pasillos[index][1][1]
-
                 else:
-
                     x = (1 - lan) * pasillos[index][0][0] + lan * pasillos[index][1][0]
                     y = (1 - lan) * pasillos[index][0][1] + lan * pasillos[index][1][1]
-
-            else:     
-
+            else:
                 x, y = pos_zs_copy[index].pop(0)
 
             ax.plot(x, y, colors_markers[c], markersize=10)
 
-
-    ax.text(40, -5, f'Tiempo: {periodo}', fontsize=12)
+    ax.text(40, -5, f'Periodo: {periodo} (5s)', fontsize=12)
 
 def mostrar_animacion_cursos(cursos):
     """Show animation for the given courses."""
     pasillos = load_pasillos('parametros_dsla_parvulario/coordenadas_pasillos.csv')
     intervalos = prepare_intervals(cursos)
     colors_markers = ['b^', 'g^', 'r^', 'c^', 'm^', 'bo', 'go', 'ro', 'co', 'mo', 'bd', 'gd', 'rd']
-    pos_zs = {-1 : [(35, 15), (35, 17), (35, 19), (35, 21), (35, 23), (35, 24), (35, 15), (35, 16), (35, 17), (35, 18), (35, 19), (35, 20), (35, 21), (35, 22), (35, 23), (35, 24)],
-              -2: [(-2, 18), (-2, 16), (-2, 14), (-2, 12), (-2, 10), (-2, 8), (-2, 6), (-2, 4), (-2, 2), (-2, 0), (-2, 18), (-2, 16), (-2, 14), (-2, 12), (-2, 10), (-2, 8), (-2, 6), (-2, 4), (-2, 2), (-2, 0)],
+    pos_zs = {-1 : [(35, 17), (35, 21), (42, 17), (42, 21), (42, 15), (35, 16), (35, 17), (35, 18), (35, 19), (35, 20), (35, 21), (35, 22), (35, 23), (35, 24)],
+              -2: [(-3, 18), (-3, 16), (-3, 14), (-3, 12), (-3, 10), (-3, 8), (-3, 6), (-3, 4), (-3, 2), (-3, 0), (-2, 18), (-2, 16), (-2, 14), (-2, 12), (-2, 10), (-2, 8), (-2, 6), (-2, 4), (-2, 2), (-2, 0)],
               -3: [(6, -1), (12, -1), (6, -3), (12, -3), (12, -3), (6, -5), (8, -5), (10, -5), (12, -5), (6, -1), (8, -1), (10, -1), (12, -1), (6, -3), (8, -3), (10, -3), (12, -3), (6, -5), (8, -5), (10, -5), (12, -5)],}
 
     pos_zs_copy = {}
-    
     for z in pos_zs:
         pos_zs_copy[z] = deepcopy(pos_zs[z])
 
     fig, ax = plt.subplots(figsize=(10, 8))
 
-    def update(t):
-        update_plot(t, ax, intervalos, cursos, pasillos, colors_markers, pos_zs=pos_zs_copy)
+    with PdfPages('animacion.pdf') as pdf:
+        def update(t):
+            update_plot(t, ax, intervalos, cursos, pasillos, colors_markers, pos_zs=pos_zs_copy)
+            pdf.savefig(fig)  # Save the current figure to the PDF
 
-    ani = FuncAnimation(fig, update, frames=range(150), interval=100)
-    ani.save(f'animacion.gif', writer='pillow', fps=10)
-    plt.show()
+        ani = FuncAnimation(fig, update, frames=range(150), interval=100)
+        plt.show()
 
 if __name__ == '__main__':
     with open('soluciones.json') as f:
